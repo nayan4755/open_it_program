07d2f633b7918f8185f0b18f134666585aa4f1f8
nayan4755 <47407889+nayan4755@users.noreply.github.com>
2020-08-28 20:15:42 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,36 @@
+import os
+
+txt = raw_input("Enter your command: ")
+
+lst = [txt]
+ 
+
+#while True:
+for x in lst:
+	if (("open" in x) or ("run" in x) or ("execute" in x)) and ("calculator" in x):
+		os.system('bash -c gnome-calculator')
+
+	elif (("open" in x) or ("run" in x) or ("execute" in x)) and ('camera' in x):
+		os.system('bash -c cheese')
+
+	elif (("open" in x) or ("run" in x) or ("execute" in x)) and ('settings' in x):
+		os.system('bash -c gnome-control-center')
+
+	elif (("open" in x) or ("run" in x) or ("execute" in x)) and ('browser' in x):
+		os.system('bash -c sensible-browser')
+	
+	elif (("open" in x) or ("run" in x) or ("execute" in x)) and ('chrome' in x):
+		os.system('bash -c sensible-browser')
+
+	elif (("open" in x) or ("run" in x) or ("execute" in x)) and ('google' in x):
+		os.system('bash -c sensible-browser')
+	
+	elif (("open" in x) or ("run" in x) or ("execute" in x)) and ('file' in x):
+		os.system('bash -c nautilus')
+
+	elif ("exit" in x) or ("quit" in x):
+		print "Have a good day!"
+		break
+
+	else:
+		print ("out of my powers")
